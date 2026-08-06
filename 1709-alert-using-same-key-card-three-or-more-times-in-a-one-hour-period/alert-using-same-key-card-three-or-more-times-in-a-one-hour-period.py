8175022a58cb5399711e871dd936eedf63162d8a
Ankush Kamble <109985321+ankushkamble93@users.noreply.github.com>
2026-08-06 12:24:06 -0400

    Time: 87 ms (93.80%) | Memory: 38.3 MB (81.40%) - LeetSync

--- 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
+++ 1709-alert-using-same-key-card-three-or-more-times-in-a-one-hour-period/alert-using-same-key-card-three-or-more-times-in-a-one-hour-period.py
@@ -1,17 +1,19 @@
 class Solution:
     def alertNames(self, keyName: List[str], keyTime: List[str]) -> List[str]:
         user_times = collections.defaultdict(list)
-        for name, time_str in zip(keyName,keyTime):
-            hour, mins = time_str.split(":")
-            total_mins = int(hour) * 60 + int(mins)
-            user_times[name].append(total_mins)
+        for name, time in zip(keyName, keyTime):
+            hour, mins = time.split(":")
+            total_time = int(hour) * 60 + int(mins)
+            user_times[name].append(total_time)
         result = []
-        for name, times in user_times.items():
-            times.sort()
-            for i in range(len(times)-2):
-                if times[i+2] - times[i] <= 60:
+        for name, time in user_times.items():
+            time.sort()
+            for i in range(len(time)-2):
+                if time[i+2] - time[i] <= 60:
                     result.append(name)
                     break
         return sorted(result)
+
+
 
         
